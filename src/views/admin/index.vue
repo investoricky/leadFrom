4d6b5b1d499634391248843aaa773b046215ce80
Dynastiy <emmanuel00michael@gmail.com>
2021-06-02 12:28:30 +0100

    Added admin and user pages

---
+++ src/views/admin/index.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <h1>This is the admin Page</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/user/index.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <h1>This is the User Page</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
