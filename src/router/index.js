44b9cf135c4354fa28f6d7efcc1983c53ed2b339
ricky <sandersakpan@gmail.com>
2021-06-28 09:55:00 +0100

    userprofile

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/user/index.vue'
+import userProfile from '../views/user/pages/userProfile.vue'
 
 Vue.use(VueRouter)
 
@@ -8,6 +9,11 @@ const routes = [{
         path: '/',
         name: 'Home',
         component: Home
+    },
+    {
+    path: '/userProfile',
+        name: 'userProfile',
+        component: userProfile
     },
 ]
 
