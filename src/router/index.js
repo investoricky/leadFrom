60f727d2254bef4cd5d9b363f89cbc73aeba5932
vivibasky <vivianeshiet@gmail.com>
2021-06-29 16:10:51 +0100

    verification first and second step implemented

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,10 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/user/index.vue'
 import userProfile from '../views/user/pages/userProfile.vue'
+import firstStep from '../views/user/verification/firstStep.vue'
+import secondStep from '../views/user/verification/secondStep.vue'
+import thirdStep from '../views/user/verification/thirdStep.vue'
+import preview from '../views/user/verification/preview.vue'
 
 Vue.use(VueRouter)
 
@@ -11,9 +15,29 @@ const routes = [{
         component: Home
     },
     {
-    path: '/userProfile',
+        path: '/userProfile',
         name: 'userProfile',
         component: userProfile
+    },
+    {
+        path: '/firstStep',
+        name: 'firstStep',
+        component: firstStep
+    },
+    {
+        path: '/secondStep',
+        name: 'secondStep',
+        component: secondStep
+    },
+    {
+        path: '/thirdStep',
+        name: 'thirdStep',
+        component: thirdStep
+    },
+    {
+        path: '/preview',
+        name: 'preview',
+        component: preview
     },
 ]
 
